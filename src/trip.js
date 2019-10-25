ebb10b9f48e38aefe1e391ed943bd4b51c25e0db
Alina Gonzalez <AGonzalez2@ucvts.tec.nj.us>
2019-10-25 13:21:39 -0400

    tripp

--- src/trip.js
+++ src/trip.js
@@ -1,10 +1,11 @@
 const readlineSync = require("readline-sync");
-const student = readlineSync.question("\nStudents: ");
-const teacher = readlineSync.question("Teachers: ");
-const buscap = readlineSync.question("Bus capactiy: ");
+const student = Number(readlineSync.question("\nStudents: "));
+const teacher = Number(readlineSync.question("Teachers: "));
+const cap = Number(readlineSync.question("Bus capactiy: "));
 
-let totalp = student + teacher
-let totalb = totalp / buscap
+let totalp = student + teacher;
+let buscap = totalp / cap
+let buses = Math.ceil(buscap*1)
 let busremain = totalp % buscap
 
 console.log("\n" + totalb + " bus(es) is (are) needed, with " + busremain)
